5eb2c1131c6af4f83a3c0eeef30996ab657fadf3
Federico Ramirez <fedra.arg@gmail.com>
2020-08-18 20:18:15 -0300

    Refactor

--- test/codegen/codegen.test.js
+++ test/codegen/codegen.test.js
@@ -2,68 +2,57 @@ const expect = require('chai').expect
 const parser = require('../../lib/parser')
 const Codegen = require('../../lib/codegen/codegen')
 
+function generate (sourcecode) {
+  const ast = parser.parse(sourcecode.trim())
+  return new Codegen().call(ast)
+}
+
 describe('Codegen', function () {
   describe('#call', function () {
-    describe('DEF', function () {
-      it('works with a function with no parameters')
+    describe('def', function () {
+      it('works with a function with no parameters', function () {
+        const result = generate(`
+def greet()
+        `)
+
+        expect(result).to.contain('function greet()')
+      })
 
       it('works with a simple function', function () {
-        const ast = parser.parse(`
+        const result = generate(`
 def greet(name:string, person:ref:Person)
-        `.trim())
-        const codegen = new Codegen()
-        const result = codegen.call(ast)
+        `)
 
         expect(result).to.contain('function greet(name as string, person ref as Person)')
         expect(result).to.contain('endfunction')
       })
 
       it('works with an array and a ref parameter', function () {
-        const ast = parser.parse(`
+        const result = generate(`
 def greet(person:ref:Person[][])
-        `.trim())
-        const codegen = new Codegen()
-        const result = codegen.call(ast)
+        `)
 
         expect(result).to.contain('function greet(person ref as Person[][])')
         expect(result).to.contain('endfunction')
       })
 
       it('works with an array parameter', function () {
-        const ast = parser.parse(`
+        const result = generate(`
 def greet(person:Person[])
-        `.trim())
-        const codegen = new Codegen()
-        const result = codegen.call(ast)
+        `)
 
         expect(result).to.contain('function greet(person as Person[])')
       })
 
       it('can return a value', function () {
-        const ast = parser.parse(`
+        const result = generate(`
 def greet(person:Person[])
   return 2
-        `.trim())
-
-        const result = new Codegen().call(ast)
+        `)
 
         expect(result).to.contain('exitfunction 2')
         expect(result).to.contain('endfunction 0')
       })
     })
-
-//     describe('Function call', function () {
-//       it('compiles properly', function () {
-//         const ast = parser.parse('(greet "mike")')
-//         const codegen = new Codegen()
-//         const result = codegen.call(ast)
-//
-//         // TODO: Type checker should not compile because there's no function definition
-//         expect(result).to.eq(`
-// SSINTERNAL__PushString("mike")
-// SSINTERNAL__Call("greet")
-//         `.trim())
-//       })
-//     })
   })
 })
