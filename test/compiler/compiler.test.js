e31c362e744c18b2d05c7952a5e5bfbf5a6b374c
Federico Ramirez <fedra.arg@gmail.com>
2020-09-13 19:51:28 -0300

    Refactor

--- test/compiler/compiler.test.js
+++ test/compiler/compiler.test.js
@@ -4,13 +4,14 @@ const expect = require('chai').expect
 const Compiler = require('../../lib/compiler/compiler')
 
 describe('compiler/compiler', function () {
-  it.only('generates an output file', function () {
+  it('generates an output file', async function () {
     const compiler = new Compiler()
-    compiler.compile({ entry: path.join(__dirname, '..', 'fixtures', 'main.ss') })
+    await compiler.compile({ entry: path.join(__dirname, '..', 'fixtures', 'main.ss') })
 
     const output = fs.readFileSync(path.join(__dirname, '..', 'fixtures', 'main.agc')).toString()
     expect(output).to.contain('foo()')
     expect(output).to.contain('bar()')
     expect(output).to.contain('baz()')
+    expect(fs.existsSync(path.join(__dirname, '..', 'fixtures', 'media', 'bytecode.byc'))).to.eq(true)
   })
 })
