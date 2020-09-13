83f109561921fbc6529974f0bc1d58f196bb4b92
Federico Ramirez <fedra.arg@gmail.com>
2020-09-13 19:33:22 -0300

    Use AGK Compiler

--- test/compiler/compiler.test.js
+++ test/compiler/compiler.test.js
@@ -4,7 +4,7 @@ const expect = require('chai').expect
 const Compiler = require('../../lib/compiler/compiler')
 
 describe('compiler/compiler', function () {
-  it('generates an output file', function () {
+  it.only('generates an output file', function () {
     const compiler = new Compiler()
     compiler.compile({ entry: path.join(__dirname, '..', 'fixtures', 'main.ss') })
 
